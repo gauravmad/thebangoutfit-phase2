41c0173f09e8f661feb4a2d2dd39d1ab43fde3c0
Gaurav Madan <gauravmadan2004@gmail.com>
2023-12-22 00:54:17 +0530

    data fetch in myaccount

--- pages/myaccount.js
+++ pages/myaccount.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import { client } from '../lib/client';
 
-export default function myaccount() {
+export default function myaccount({userdetails}) {
   return (
     <div>
       My Account
+      {console.log(userdetails)}
+      {userdetails && userdetails[0] &&(
+        <h2>{userdetails[0].firstname}</h2>
+      )}
     </div>
   )
+}
+
+
+export const getServerSideProps = async ()=>{
+
+  const userquery = '*[_type == "userdetails"]';
+  const userdetails = await client.fetch(userquery);
+
+  const productquery = '*[_type == "product"]';
+  const products = await client.fetch(productquery)
+
+  const categoryquery = '*[_type == "category"]';
+  const category = await client.fetch(categoryquery);
+
+  return{
+    props:{products, category, userdetails}
+  }
 }
