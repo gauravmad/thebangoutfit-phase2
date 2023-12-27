02bdea3c5244c82239a747bdfd27fefc18e32716
reenaZ11 <vermareena3011@gmail.com>
2023-12-28 01:48:29 +0530

    about us

--- components/HomeSection/HomeBanner.js
+++ components/HomeSection/HomeBanner.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from "react";
 
 export default function HomeBanner() {
   return (
-    <div className='background w-[90%] mx-auto h-[75vh] rounded-md shadow-md bg-pink-200 flex flex-col justify-center  my-[4vh]'>
-      <div className='flex flex-col pl-[15vh] w-[55vw]  '>
-      <h1 className='text-[8vh] font-bold text-pink-600  '>"Discover Your Style"</h1>
-      <p className='text-[4vh] font-bold text-[#C68F70] px-[2vh]'>Explore the Latest Trends in Fashion</p>
-      <p className='text-[2.2vh] font-bold text-gray-600 py-[5vh] px-[2vh]'> Embark on a journey of self-expression through fashion with our latest collection. Immerse yourself in a world where style knows no bounds, and individuality reigns supreme. Our carefully curated pieces are designed to empower and inspire, elevating your everyday wardrobe to extraordinary heights.</p>
+    <div className="background w-[90%] mx-auto md:h-[75vh] rounded-md shadow-md bg-pink-200 flex flex-col justify-center  my-[4vh]">
+      <div className="flex flex-col md:pl-[15vh] md:w-[55vw] w-[60vw] p-[2vh] justify-center items-start">
+        <h1 className="md:text-[8vh] text-[3vh] font-bold text-pink-600 ">
+          Discover Your Style
+        </h1>
+        <p className="md:text-[4vh] font-medium text-[#C68F70]  ">
+          Explore the Latest Trends in Fashion
+        </p>
+        <p className="md:text-[2.2vh] text-[1.5vh]  text-gray-500 my-[2vh]">
+          Embark on a journey of self-expression through fashion with our latest
+          collection. Immerse yourself in a world where style knows no bounds,
+          and individuality reigns supreme. Our carefully curated pieces are
+          designed to empower and inspire, elevating your everyday wardrobe to
+          extraordinary heights.
+        </p>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
-  
